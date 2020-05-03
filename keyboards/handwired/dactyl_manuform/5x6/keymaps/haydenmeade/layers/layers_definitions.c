0cfbfb9c1a266983ffe56f70c2f475b954c42c0c
hayden <hayden@DESKTOP-EUHS0NF.localdomain>
2020-05-03 19:24:55 +1000

    initial layout

---
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/haydenmeade/layers/layers_definitions.c
@@ -0,0 +1,5 @@
+enum layers {        // Dvorak keyboard layers:
+    DVORAK   = 0,    //   * Dvorak base layer.
+    NUMPAD   = 1,    //   * numpad with symbols and letters that should suffice to input any numeric literal.
+    MOUSE    = 2,    //   * Mouse movement keys and edition shortcuts.
+};
